29e4fd5fa8a7b2b3d4951cbcd83c9ba05f187ebe
George Petrou <tedroark7@gmail.com>
2020-07-07 03:22:59 +0300

    Update install.sh

--- django_project/installation/install.sh
+++ django_project/installation/install.sh
@@ -4,6 +4,7 @@ pip3 install -r requirements.txt
 apt install default-jre
 apt install openjdk-11-jre-headless
 apt install openjdk-8-jre-headless
+apt-get install sqlite3
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.8.0-linux-x86_64.tar.gz
 tar -xvzf elasticsearch-7.8.0-linux-x86_64.tar.gz
 mv elasticsearch-7.8.0 ../MovieRama/
